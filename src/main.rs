bcd80e0815b6171c7fd12a5b45d9e947e215accb
1Step621 <onestep621@gmail.com>
2025-08-11 15:42:21 +0900

    chore: deploy to render.com

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use std::convert::Infallible;
+use std::{convert::Infallible, env};
 
 use axum::{http::StatusCode, response::IntoResponse, routing::get, Extension, Json, Router};
+use dotenvy::dotenv;
 use state::State;
+use tokio::net::TcpListener;
 
 mod periodic;
 mod state;
@@ -11,14 +13,22 @@ async fn get_state(Extension(state): Extension<State>) -> Result<impl IntoRespon
     Ok((StatusCode::OK, Json(contests)))
 }
 
-#[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
+#[tokio::main]
+async fn main() {
+    dotenv().expect("Failed to load .env file");
+
     let state = State::default();
 
     tokio::spawn(periodic::wait(state.clone()));
 
-    let router = Router::new()
+    let app = Router::new()
         .route("/", get(get_state))
         .layer(Extension(state));
-    Ok(router.into())
+
+    let host = env::var("HOST").expect("Missing HOST environment variable");
+    let port = env::var("PORT").expect("Missing PORT environment variable");
+    let listener = TcpListener::bind(format!("{}:{}", host, port))
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
